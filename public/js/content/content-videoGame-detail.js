32a992b58a462791a048f60cfecd19e30cc0ecfc
VictorCHX <victorlafargechx@yahoo.fr>
2021-10-15 20:47:28 +0000

    add style btn add photo and video

--- public/js/content/content-videoGame-detail.js
+++ public/js/content/content-videoGame-detail.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
     let active_img_open = document.querySelector('.active_img_open');
 
 
-
+    let container_detail_game_content_all_notice_card = document.querySelector('.container_detail_game_content_all_notice_card');
 
 
     // ----------------------- Display img open
@@ -74,6 +74,60 @@ document.addEventListener('DOMContentLoaded', ()=> {
     
 
     console.log(all_game_detail_small_img);
+
+
+
+
+
+
+
+
+
+    // ------------------------ Scroll Notices Cards
+    const elNoticesScroll = container_detail_game_content_all_notice_card;
+    elNoticesScroll.style.cursor = "grab";
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+
+    const mouseDownHandler = function (e) {
+        elNoticesScroll.style.cursor = 'grabbing';
+        elNoticesScroll.style.userSelect = 'none';
+
+        pos = {
+            left: elNoticesScroll.scrollLeft,
+            top: elNoticesScroll.scrollTop,
+            // Get the current mouse position
+            x: e.clientX,
+            y: e.clientY,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
+
+        // Scroll the element
+        elNoticesScroll.scrollTop = pos.top - dy;
+        elNoticesScroll.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function () {
+        elNoticesScroll.style.cursor = 'grab';
+        elNoticesScroll.style.removeProperty('user-select');
+
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    
+    elNoticesScroll.addEventListener('mousedown', mouseDownHandler);
+
+
 
 
 
