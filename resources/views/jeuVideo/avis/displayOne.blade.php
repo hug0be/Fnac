2d0f1b8d3845fe56c28fd237300a9ea594fd0901
VictorCHX <victorlafargechx@yahoo.fr>
2021-10-21 14:18:20 +0000

    [FEATURE] Added form Notice Useful form

--- resources/views/jeuVideo/avis/displayOne.blade.php
+++ resources/views/jeuVideo/avis/displayOne.blade.php
@@ -5,4 +5,18 @@
     <p class="notice_card_date">Avis posté le {{ $aNotice->avi_date->translatedFormat('l jS F Y à H\hi') }}</p>
     <p class="notice_card_title"> {{ $aNotice->avi_titre }} </p>
     <p class="notice_card_descr"> {{ $aNotice->avi_detail }} </p>
+
+    <p class="container_notice_useful">
+        <p class="notice_useful_txt">Cet avis vous a été utile ?</p>
+
+        <div class="container_notice_useful_answer">
+            <form action="">
+                <input type="submit" value="Oui" class="notice_useful_answer notice_useful_answer_yes">
+            </form>
+
+            <form action="">
+                <input type="submit" value="Oui" class="notice_useful_answer notice_useful_answer_no">
+            </form>
+        </div>
+    </p>
 </div>
