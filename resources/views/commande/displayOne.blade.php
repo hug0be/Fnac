2dcd8ac14e72a02dcddf4e7ede5d0c775b987c5b
VictorCHX <victorlafargechx@yahoo.fr>
2021-10-30 09:17:55 +0000

    Modif style état livraison

--- resources/views/commande/displayOne.blade.php
+++ resources/views/commande/displayOne.blade.php
@@ -32,7 +32,7 @@
 
                 <div class="infoComm_content_delivery">
                     <h5 class="infoComm_content_delivery_title">
-                        Livraison : {{ $aCommande->typeDelivery() }} - <span class="etat_livraison">{{ $aCommande->displayEtat() }}</span>
+                        Livraison : {{ $aCommande->typeDelivery() }} - <span class="etat_livraison {{ $aCommande->displayEtat() }}">{{ $aCommande->displayEtat() }}</span>
                     </h5>
 
                    
